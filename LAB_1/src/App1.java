828ff9d855939e8c7bf15acdca2228831696aaa1
Stas123456789k <stanislavklimovich3@gmail.com>
2025-12-26 05:06:38 +0300

    Изменил LAB_1 и добавил настройки проектов

--- LAB_1/src/App1.java
+++ LAB_1/src/App1.java
@@ -3,6 +3,6 @@ public class App1
     public static void main(String[]args)
     {
         int x = 4 * 4 - 2 * 4; //  (int) Math.Pow (4,2) - 2 * 4
-        System.out.println("X = " + x);
+        System.out.println("X =! " + x);
     }
 }
